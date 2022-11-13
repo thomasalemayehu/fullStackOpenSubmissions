b67a74620f147ea6abfcac9193a3359f0c339481
Thomas Alemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-11-13 06:42:31 +0300

    anecdotes : 1.12, 1.13, 1.14 added

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+  });
+
+
+  const generateRandomNumber = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length - 1) + 1);
+
+  const getAnecdoteWithLargestVote = () => {
+    var indexOfAnecdoteWithMaxVote = 0;
+    var numberOfMaxVote = 0;
+   for (const [key, value] of Object.entries(votes)) {
+     if(value > numberOfMaxVote){
+      numberOfMaxVote = value;
+      indexOfAnecdoteWithMaxVote = key;
+     }
+   }
+
+   return anecdotes[indexOfAnecdoteWithMaxVote];
+  };
+
+  const voteForAnecdote = (key) => {
+    const newVotes = { ...votes };
+
+    newVotes[key] += 1;
+
+    setVotes(newVotes);
+    console.log("Voting", newVotes);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <div>Vote : {votes[selected]}</div>
+
+      <button onClick={generateRandomNumber}>New Anecdote</button>
+      <button
+        onClick={() => {
+          voteForAnecdote(selected);
+        }}
+      >
+        Vote
+      </button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{getAnecdoteWithLargestVote()}</p>
     </div>
   );
 }
